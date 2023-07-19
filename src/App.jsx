763d2cd6979c46444c42ca7a12389fcee45976b4
Yeison Andrés Marroquín Bernal <121591987+yeisonmarroquin887@users.noreply.github.com>
2023-07-18 21:07:25 -0500

    CEASOL

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
   return (
     <div className='App'>
       {
-        false
+        Loading
         ?<Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/nosotros' element={<Nosotros/>}/>
